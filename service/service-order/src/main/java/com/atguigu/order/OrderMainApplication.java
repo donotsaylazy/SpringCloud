338ae90d511a93132f558f43492bf5abdf0f931a
donotsaylazy <769031940@qq.com>
2025-02-24 17:23:20 +0800

    初始化项目

---
+++ service/service-order/src/main/java/com/atguigu/order/OrderMainApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.order;
+
+/**
+ * @PackageName: com.atguigu.order
+ * @ClaseName: OrderMainApplication
+ * @Description: OrderMainApplication
+ * @Author: zhaoyg
+ * @Date: 2025/2/24 17:18
+ * @params *@params $
+ */
+
+@SpringBootApplication
+public class OrderMainApplication {
+}
